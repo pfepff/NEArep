e31c933743e2c18f8ef8c682c16b5051fd3748a4
pfepff <137122744+pfepff@users.noreply.github.com>
2023-12-14 21:42:32 +0000

    Changed from dy & dx to using speed and angle
    Changed from having two speeds: dy and dx, to one speed and an object specific angle

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,63 +1,61 @@
 var canvas = document.querySelector("canvas");
-canvas.width = (window.innerWidth / 1.3);
-canvas.height = (window.innerHeight / 1.2);
+canvas.width = window.innerWidth / 1.3;
+canvas.height = window.innerHeight / 1.2;
 
 var c = canvas.getContext("2d");
 
 class PreyBoid {
-    constructor() {
-        this.radius = 9;
-        this.x = Math.random() * (canvas.width - this.radius * 2) + this.radius;
-        this.y = Math.random() * (canvas.height - this.radius * 2) + this.radius;
-        this.dx = (Math.random() - 0.5) * 10;
-        this.dy = (Math.random() - 0.5) * 10;
-    }
+  constructor() {
+    this.radius = 9;
+    this.x = Math.random() * (canvas.width - this.radius * 2) + this.radius;
+    this.y = Math.random() * (canvas.height - this.radius * 2) + this.radius;
+    this.speed = 5; // Set the speed
+    this.angle = Math.random() * Math.PI * 2; // Set the angle
+  }
 
-    drawPreyBoid() {
-        c.strokeStyle = "black";
-        c.fillStyle = "green";
-//Triangle
-        c.beginPath();
-        // Calculate the angle of the normalized direction vector
-        let angle = Math.atan2(this.dy, this.dx);
-        // Draw the triangle
-        c.moveTo(this.x + this.radius * Math.cos(angle), this.y + this.radius * Math.sin(angle));
-        c.lineTo(this.x + this.radius * Math.cos(angle - (3 * Math.PI / 4)), this.y + this.radius * Math.sin(angle - (3 * Math.PI / 4)));
-        c.lineTo(this.x + this.radius * Math.cos(angle + (3 * Math.PI / 4)), this.y + this.radius * Math.sin(angle + (3 * Math.PI / 4)));
-        c.closePath();
-        c.stroke();
-        c.fill();
-    }
+  drawPreyBoid() {
+    c.strokeStyle = "black";
+    c.fillStyle = "green";
+    // Triangle
+    c.beginPath();
+    c.moveTo(this.x + this.radius * Math.cos(this.angle), this.y + this.radius * Math.sin(this.angle));
+    c.lineTo(this.x + this.radius * Math.cos(this.angle - (3 * Math.PI / 4)), this.y + this.radius * Math.sin(this.angle - (3 * Math.PI / 4)));
+    c.lineTo(this.x + this.radius * Math.cos(this.angle + (3 * Math.PI / 4)), this.y + this.radius * Math.sin(this.angle + (3 * Math.PI / 4)));
+    c.closePath();
+    c.stroke();
+    c.fill();
+  }
 
-    takeStep() {
-//Edge collision and movement X
-        if ((this.x + this.radius) > canvas.width || (this.x - this.radius) < 0) {
-        this.dx = -this.dx;
-        }
-        this.x += this.dx;
-//Edge collision and movement Y
-        if ((this.y + this.radius) > canvas.height || (this.y - this.radius) < 0) {
-            this.dy = -this.dy;
-        }
-        this.y += this.dy;
+  takeStep() {
+    // Edge collision and movement
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.angle = Math.PI - this.angle; // Reverse the angle
     }
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.angle = -this.angle; // Reverse the angle
+    }
+    // Update the position based on the angle and speed
+    this.x += this.speed * Math.cos(this.angle);
+    this.y += this.speed * Math.sin(this.angle);
+  }
 }
 
 var PreyArray = [];
 var numPrey = 20;
 
 for (var i = 0; i < numPrey; i++) {
-    var boid = new PreyBoid();
-    PreyArray.push(boid);
+  var boid = new PreyBoid();
+  PreyArray.push(boid);
 }
 
 function update() {
-    requestAnimationFrame(update);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(update);
+  c.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < PreyArray.length; i++) {
-        PreyArray[i].takeStep();
-        PreyArray[i].drawPreyBoid();
-    }
+  for (var i = 0; i < PreyArray.length; i++) {
+    PreyArray[i].takeStep();
+    PreyArray[i].drawPreyBoid();
+  }
 }
+
 update();
