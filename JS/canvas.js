3855720fc40e7a866c7a8a399658d3d337967d6d
pfepff <137122744+pfepff@users.noreply.github.com>
2023-12-15 11:02:55 +0000

    Added vision cones
    Purely visula, haha gettit (but i do have to add some functionality soon)

--- JS/canvas.js
+++ JS/canvas.js
@@ -9,12 +9,21 @@ class PreyBoid {
     this.radius = 9;
     this.x = Math.random() * (canvas.width - this.radius * 2) + this.radius;
     this.y = Math.random() * (canvas.height - this.radius * 2) + this.radius;
-    this.speed = 5; // Set the speed
+    this.speed = 20; // Set the speed
     this.angle = Math.random() * Math.PI * 2; // Set the angle
+    this.visRadius = 50;
+    this.visAngle = 2;
+  }
+
+  drawPreyVisionCone() {
+    c.fillStyle = "red"
+    c.moveTo(this.x,this.y);
+    c.arc(this.x,this.y,this.visRadius, this.angle - this.visAngle, this.angle + this.visAngle);
+    c.lineTo(this.x,this.y);
+    c.fill();
   }
 
   drawPreyBoid() {
-    c.strokeStyle = "black";
     c.fillStyle = "green";
     // Triangle
     c.beginPath();
@@ -22,18 +31,39 @@ class PreyBoid {
     c.lineTo(this.x + this.radius * Math.cos(this.angle - (3 * Math.PI / 4)), this.y + this.radius * Math.sin(this.angle - (3 * Math.PI / 4)));
     c.lineTo(this.x + this.radius * Math.cos(this.angle + (3 * Math.PI / 4)), this.y + this.radius * Math.sin(this.angle + (3 * Math.PI / 4)));
     c.closePath();
-    c.stroke();
     c.fill();
+    this.drawPreyVisionCone()
   }
 
-  takeStep() {
-    // Edge collision and movement
+  edgeCollision() {
+    //Edge collision
+    // Bounce
     if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
       this.angle = Math.PI - this.angle; // Reverse the angle
     }
     if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
       this.angle = -this.angle; // Reverse the angle
     }
+
+    // // Pacman
+    // if (this.x > canvas.width) {
+    //   this.x = 0;
+    // }
+    // if (this.x < 0) {
+    //   this. x = canvas.width;
+    // }
+    // if (this.y > canvas.height) {
+    //   this.y = 0
+    // }
+    // if (this.y < 0) {
+    //   this.y = canvas.height
+    // }
+  }
+
+
+  takeStep() {
+    this.edgeCollision()
+
     // Update the position based on the angle and speed
     this.x += this.speed * Math.cos(this.angle);
     this.y += this.speed * Math.sin(this.angle);
@@ -49,7 +79,10 @@ for (var i = 0; i < numPrey; i++) {
 }
 
 function update() {
-  requestAnimationFrame(update);
+//  document.onkeypress = function (e) {
+//    e = e || window.event;
+    requestAnimationFrame(update);
+//  };
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   for (var i = 0; i < PreyArray.length; i++) {
