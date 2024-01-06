a95394bc4f3dd3724df300e5cc1391125c1c39d4
pfepff <137122744+pfepff@users.noreply.github.com>
2024-01-06 22:00:34 +0000

    how does github work
    i need help or like a tutorial maybe

--- JS/testing.js
+++ JS/testing.js
@@ -1,33 +1,10 @@
 var canvas = document.querySelector("canvas");
-<<<<<<< Updated upstream
-canvas.width = window.innerWidth / 1.3;
-canvas.height = window.innerHeight / 1.2;
-=======
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
->>>>>>> Stashed changes
 
 var c = canvas.getContext("2d");
 class PreyBoid {
   constructor() {
-<<<<<<< Updated upstream
-    this.radius = 9;
-    this.x = Math.random() * (canvas.width - this.radius * 2) + this.radius;
-    this.y = Math.random() * (canvas.height - this.radius * 2) + this.radius;
-    this.speed = 20; // Set the speed
-    this.angle = Math.random() * Math.PI * 2; // Set the angle
-    this.visRadius = 100;
-    this.visAngle = 2;
-  }
-
-  drawPreyVisionCone() {
-    c.fillStyle = "rgba(255, 0, 0, 0.2)";
-    c.moveTo(this.x,this.y);
-    c.arc(this.x,this.y,this.visRadius, this.angle - this.visAngle, this.angle + this.visAngle);
-    c.lineTo(this.x,this.y);
-    c.closePath();
-    c.fill();
-=======
     this.size = 9;
     this.pos = {
       x:canvas.width/2, //Math.random() * (canvas.width - this.size * 2) + this.size,
@@ -50,7 +27,6 @@ class PreyBoid {
        c.lineTo(this.pos.x,this.pos.y);
        c.closePath();
        c.fill();
->>>>>>> Stashed changes
   }
 
   drawPreyBoid() {
@@ -58,15 +34,9 @@ class PreyBoid {
   
     c.fillStyle = "green";
     c.beginPath();
-<<<<<<< Updated upstream
-    c.moveTo(this.x + this.radius * Math.cos(this.angle), this.y + this.radius * Math.sin(this.angle));
-    c.lineTo(this.x + this.radius * Math.cos(this.angle - (3 * Math.PI / 4)), this.y + this.radius * Math.sin(this.angle - (3 * Math.PI / 4)));
-    c.lineTo(this.x + this.radius * Math.cos(this.angle + (3 * Math.PI / 4)), this.y + this.radius * Math.sin(this.angle + (3 * Math.PI / 4)));
-=======
     c.moveTo(this.pos.x + Math.cos(this.angle) * this.size, this.pos.y + Math.sin(this.angle) * this.size);
     c.lineTo(this.pos.x + Math.cos(this.angle - (Math.PI * 2 / 3)) * this.size, this.pos.y + Math.sin(this.angle - (Math.PI * 2 / 3)) * this.size);
     c.lineTo(this.pos.x + Math.cos(this.angle + (Math.PI * 2 / 3)) * this.size, this.pos.y + Math.sin(this.angle + (Math.PI * 2 / 3)) * this.size);
->>>>>>> Stashed changes
     c.closePath();
     c.fill();
   }
@@ -76,18 +46,6 @@ class PreyBoid {
     this.pos.y = (this.pos.y + canvas.height) % canvas.height;
   }
 
-<<<<<<< Updated upstream
-  takeStep() {
-    this.edgeCollision()
-    this.PreyPercieve
-    //seperate
-    //allign
-    //cohere
-
-    // Update the position based on the angle and speed
-    this.x += this.speed * Math.cos(this.angle);
-    this.y += this.speed * Math.sin(this.angle);
-=======
   normalizeAngle(){
     this.angle = (this.angle % (2 * Math.PI) + 2 * Math.PI) % (2 * Math.PI);
   }
@@ -105,7 +63,6 @@ class PreyBoid {
     this.pos.x += this.speedVector.dx; // Update the x position using dx
     this.pos.y += this.speedVector.dy; // Update the y position using dy
     this.angle = Math.atan2(this.speedVector.dy, this.speedVector.dx);
->>>>>>> Stashed changes
   }
 }
 
@@ -118,17 +75,10 @@ for (var i = 0; i < numPrey; i++) {
 }
 
 function update() {
-<<<<<<< Updated upstream
-//  document.onkeypress = function (e) {
-//    e = e || window.event;
-    requestAnimationFrame(update);
-//  };
-=======
   //document.onkeypress = function (e) {
   //  e = e || window.event;
     requestAnimationFrame(update);
   //};
->>>>>>> Stashed changes
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   for (var i = 0; i < PreyArray.length; i++) {
