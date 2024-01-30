902d2654ea82e26ed4da935c78f0a93f14b26ff1
pfepff <137122744+pfepff@users.noreply.github.com>
2024-01-30 17:26:11 +0000

    done?
    think so...

--- JS/PreyBoid.js
+++ JS/PreyBoid.js
@@ -1,130 +1,258 @@
-import {c, canvas, preyArray} from './canvas.js';
+import {c, canvas, preyArray, settings, seperationValue, cohesionValue, alignmentValue, visRadius, avoidRadius, wallAvoidance, maxSpeed, minSpeed} from './canvas.js';
 
 class PreyBoid{
-    constructor() {
-      this.size = 9;
-      this.pos = {
-        x: Math.random() * (canvas.width - this.size * 2) + this.size,
-        y: Math.random() * (canvas.height - this.size * 2) + this.size
-      };
-      this.velocity = {
-        dx: (Math.random() - 0.5) * 20,
-        dy: (Math.random() - 0.5) * 20 
-      };
-      this.visRadius = 250;
-      this.visAngle = Math.PI * 2;
+  constructor() {
+    this.Frame_Buffer = 1;
+    this.stut = 0;
 
-      this.seperationConstant = 0.01;
-      this.cohesionConstant = 0.1;
-      this.alignmentConstant = 0.1;
+    this.size = 4;
+    this.pos = {
+      x: Math.random() * (canvas.width - this.size * 2) + this.size,
+      y: Math.random() * (canvas.height - this.size * 2) + this.size
+    };
+    this.velocity = {
+      dx: Math.random() < 0.5 ? -maxSpeed/2 : maxSpeed/2,
+      dy: Math.random() < 0.5 ? -maxSpeed/2 : maxSpeed/2
+    };
 
-      this.maxVelocity = 1;
-      this.minVeclocity = 1;
+    this.boidsSpotted = [];
+    this.boidsTooClose = [];
+  }
+
+  preyDrawBoid() {
+    if (settings.drawVisionCone) {
+      this.preyDrawVisionCone();
     }
-  
-    preyDrawBoid() {
-      if (this == preyArray[0]) {
-        this.preyDrawVisionCone();
+    if (settings.drawavoidanceCone) {
+      this.preyDrawAvoidanceCone();
+    }
+    if (settings.drawForceVector) {
+      this.preyDrawForceVectors();
+    }
+
+    let redValue = Math.min(255, this.speed * 255 / maxSpeed);
+    let greenValue = Math.min(255, 255 - redValue);
+
+    if (settings.highlightBoid) {
+      if (this == preyArray[0]){
+        c.fillStyle = `rgba(255, 0, 0, 1)`;
       }
-    
-      // Calculate color based on speed
-      let redValue = Math.min(255, this.speed * 20);
-      let greenValue = Math.min(255, 255 - redValue);
-
-      c.fillStyle = `rgb(${greenValue},${redValue},0)`;
-      c.beginPath();
-      c.moveTo(this.pos.x + Math.cos(this.angle) * this.size, this.pos.y + Math.sin(this.angle) * this.size);
-      c.lineTo(this.pos.x + Math.cos(this.angle - (Math.PI * 2 / 3)) * this.size, this.pos.y + Math.sin(this.angle - (Math.PI * 2 / 3)) * this.size);
-      c.lineTo(this.pos.x + Math.cos(this.angle + (Math.PI * 2 / 3)) * this.size, this.pos.y + Math.sin(this.angle + (Math.PI * 2 / 3)) * this.size);
-      c.closePath();
-      c.fill();
+      else {
+        c.fillStyle = `rgb(${greenValue}, ${redValue}, 0, 0.2)`;
+      }
     }
-  
-    preyDrawVisionCone() {
-      c.fillStyle = "rgba(255, 0, 0, 0.1)";
+
+    else {
+      c.fillStyle = `rgb(${greenValue},${redValue},0)`;
+    }
+
+    c.beginPath();
+    c.moveTo(this.pos.x + Math.cos(this.angle) * this.size, this.pos.y + Math.sin(this.angle) * this.size);
+    c.lineTo(this.pos.x + Math.cos(this.angle - (Math.PI * 2 / 3)) * this.size, this.pos.y + Math.sin(this.angle - (Math.PI * 2 / 3)) * this.size);
+    c.lineTo(this.pos.x + Math.cos(this.angle + (Math.PI * 2 / 3)) * this.size, this.pos.y + Math.sin(this.angle + (Math.PI * 2 / 3)) * this.size);
+    c.closePath();
+    c.fill();
+  }
+
+  preyDrawForceVectors() {
+    if (this == preyArray[0]) {
+      c.strokeStyle = "rgb(255, 255, 255)";
       c.beginPath();
       c.moveTo(this.pos.x,this.pos.y);
-      c.arc(this.pos.x, this.pos.y, this.visRadius, this.angle - (this.visAngle / 2), this.angle + (this.visAngle / 2));
+      c.lineTo(this.pos.x + this.velocity.dx * 20, this.pos.y);
+      c.moveTo(this.pos.x,this.pos.y);
+      c.lineTo(this.pos.x, this.pos.y + this.velocity.dy * 20);
+      c.closePath();
+      c.stroke();
+    }
+  }
+
+  preyDrawVisionCone() {
+    if (this == preyArray[0]) {
+      c.fillStyle = "rgba(255, 255, 255, 0.1)";
+      c.beginPath();
+      c.moveTo(this.pos.x,this.pos.y);
+      c.arc(this.pos.x, this.pos.y, visRadius, this.angle - (Math.PI*2 / 2), this.angle + (Math.PI*2 / 2));
+      c.lineTo(this.pos.x,this.pos.y);
+      c.closePath();
+      c.fill();
+      c.fillStyle = "rgba(0, 0, 0, 0.2)";
+      c.beginPath();
+      c.moveTo(this.pos.x,this.pos.y);
+      c.arc(this.pos.x, this.pos.y, avoidRadius, this.angle - (avoidRadius / 2), this.angle + (avoidRadius / 2));
       c.lineTo(this.pos.x,this.pos.y);
       c.closePath();
       c.fill();
     }
+  }
 
-    preySeperate() {
-        this.velocity.dx -= Math.cos(this.angleBetween) * this.speed * this.seperationConstant;
-        this.velocity.dy -= Math.sin(this.angleBetween) * this.speed * this.seperationConstant;
+  preyDrawAvoidanceCone() {
+    if (this == preyArray[0]) {
+      c.fillStyle = "rgba(255, 255, 255, 0.1)";
+      c.beginPath();
+      c.moveTo(this.pos.x,this.pos.y);
+      c.arc(this.pos.x, this.pos.y, avoidRadius, this.angle - (Math.PI*2 / 2), this.angle + (Math.PI*2 / 2));
+      c.lineTo(this.pos.x,this.pos.y);
+      c.closePath();
+      c.fill();
     }
+  }
 
-    preyAlign() {
-        // point towards mean of angle of boids around it, could speed up if its further away based on the dot product (might make them too snappy) 
+  drawLineToVis() {
+    if (this == preyArray[0]) {
+      c.strokeStyle = "rgba(255, 255, 0, 0.5)";
+      c.beginPath();
+      c.moveTo(this.pos.x,this.pos.y);
+      c.lineTo(this.pos.x + this.distX,this.pos.y + this.distY)
+      c.closePath();
+      c.stroke();
     }
+  }
 
-    preyCohere() {
-        // travel toward centre of mass of closest boids
+  drawLineToavoidance() {
+    if (this == preyArray[0]) {
+      c.strokeStyle = "rgba(255, 0, 0, 0.5)";
+      c.beginPath();
+      c.moveTo(this.pos.x,this.pos.y);
+      c.lineTo(this.pos.x + this.distX,this.pos.y + this.distY)
+      c.closePath();
+      c.stroke();
     }
+  }
 
-    preyResolve() {
-      this.preySeperate();
-      this.preyAlign();
-      this.preyCohere();
+  preySeperate() {
+    for (var i = 0; i < this.boidsTooClose.length; i++) {
+      this.avoidanceVector = {
+        x : this.pos.x - this.boidsTooClose[i].pos.x,
+        y : this.pos.y - this.boidsTooClose[i].pos.y
+      }
+      this.velocity.dx += this.avoidanceVector.x * seperationValue;
+      this.velocity.dy += this.avoidanceVector.y * seperationValue;
     }
+  }
 
-    preySight() {
-      for (var i = 0; i < preyArray.length; i++) {
-        this.distX = preyArray[i].pos.x - this.pos.x;
-        this.distY = preyArray[i].pos.y - this.pos.y;
-        this.distBetween = Math.sqrt(this.distX ** 2 + this.distY ** 2);
-        this.angleBetween = Math.atan2(this.distY, this.distX);
+  preyAlign() {
+    this.MeanVelocity = {
+      dx : 0,
+      dy : 0
+    }
+    if (this.boidsSpotted.length > 0) {
+      for (var i = 0; i < this.boidsSpotted.length; i++) {
+        this.MeanVelocity.dx += this.boidsSpotted[i].velocity.dx;
+        this.MeanVelocity.dy += this.boidsSpotted[i].velocity.dy;
+      }
+      this.MeanVelocity.dx /= this.boidsSpotted.length;
+      this.MeanVelocity.dy /= this.boidsSpotted.length;
 
-        /*
-        cos(a) = (a0*b1 + b0*b1) / (|a0|*|b1| + |b0|*|b1|)
+      this.velocity.dx += (this.MeanVelocity.dx - this.velocity.dx) * alignmentValue;
+      this.velocity.dy += (this.MeanVelocity.dy - this.velocity.dy) * alignmentValue;
+    }
+  }
 
-        a = acos((a0*b1 + b0*b1) / (|a0|*|b1| + |b0|*|b1|))
+  preyCohere() {
+    this.MeanPosistion = {
+      x : 0,
+      y : 0
+    }
+    if (this.boidsSpotted.length > 0) {
+      for (var i = 0; i < this.boidsSpotted.length; i++){
+        this.MeanPosistion.x += this.boidsSpotted[i].pos.x;
+        this.MeanPosistion.y += this.boidsSpotted[i].pos.y;
+      }
+      this.MeanPosistion.x /= this.boidsSpotted.length;
+      this.MeanPosistion.y /= this.boidsSpotted.length;
+      
+      this.velocity.dx += (this.MeanPosistion.x - this.pos.x) * cohesionValue;
+      this.velocity.dy += (this.MeanPosistion.y - this.pos.y)* cohesionValue;
+    }
+  }
 
-        a = [this.vecocity.dx, this.vecocity.dy]
-        a = [PreyArray[i].vecocity.dx, PreyArray[i].vecocity.dy]
+  preyResolve() {
+    this.preySeperate();
+    this.preyAlign();
+    this.preyCohere();
+    this.boidsSpotted.length = 0;
+    this.boidsTooClose.length = 0;
+  }
 
-        if a = this, b = that:
-          a = acos((this.velocity.dx * preyArray[i].velocity.dy + preyArray[i].velocity.dx * this.velocity.dy) / |this.velocity.dx| * |preyArray[i].velocity.dy| + |preyArray[i].velocity.dx|)
+  preySight() {
+    for (var i = 0; i < preyArray.length; i++) {
+      this.distX = preyArray[i].pos.x - this.pos.x;
+      this.distY = preyArray[i].pos.y - this.pos.y;
+      this.distBetween = Math.sqrt(this.distX ** 2 + this.distY ** 2);
+      this.angleBetween = Math.atan2(this.distY, this.distX);
 
-        */
-//        if (this == preyArray[0]) {
-          if (this.distBetween <= this.visRadius && this !== preyArray[i]) {
-            this.preyResolve();
-          }
-//        }
+      /*cos(a) = (a0*b1 + b0*b1) / (|a0|*|b1| + |b0|*|b1|)#a = acos((a0*b1 + b0*b1) / (|a0|*|b1| + |b0|*|b1|))#a = [this.vecocity.dx, this.vecocity.dy]#a = [PreyArray[i].vecocity.dx, PreyArray[i].vecocity.dy]#if a = this, b = that:#a = acos((this.velocity.dx * preyArray[i].velocity.dy + preyArray[i].velocity.dx * this.velocity.dy) / |this.velocity.dx| * |preyArray[i].velocity.dy| + |preyArray[i].velocity.dx|)*/
+      if (this.distBetween <= visRadius && this.distBetween >= avoidRadius && this !== preyArray[i]) {
+        this.boidsSpotted.push(preyArray[i]);
+        if (settings.drawLineTo) {
+          this.drawLineToVis();
+        }
+      }
+      if (this.distBetween <= avoidRadius && this !== preyArray[i]) {
+        this.boidsTooClose.push(preyArray[i]);
+        if (settings.drawLineTo) {
+          this.drawLineToavoidance();
+        }
       }
     }
+    this.preyResolve();
+  }
 
-    preyEdgeHandling() {
+  preyEdgeHandling() {
+    if (settings.avoidWalls) {
+// Left
+      if (this.pos.x < 0 + visRadius) {
+        this.velocity.dx += Number(wallAvoidance);
+      }
+// Right
+      if (this.pos.x > canvas.width - visRadius) {
+        this.velocity.dx -= Number(wallAvoidance);  
+      }
+// Top
+      if (this.pos.y < 0 + visRadius) {
+        this.velocity.dy += Number(wallAvoidance);
+      }
+// Bottom
+      if (this.pos.y > canvas.height - visRadius) {
+        this.velocity.dy -= Number(wallAvoidance);
+      }
+    }
+    else {
       this.pos.x = (this.pos.x + canvas.width) % canvas.width;
       this.pos.y = (this.pos.y + canvas.height) % canvas.height;
     }
+  }
 
-    preyMinMaxVelocity() {
-      if (this.speed > this.maxVelocity || this.speed < -this.maxVelocity) {
-        while (1 == 1){
-          this.velocity.dx = 0;
-          console.log("caught")
-        }
-      }
-      if (this.speed < this.minVeclocity && this.speed > -this.minVeclocity) {
-        
-      }
+  preyMinMaxVelocity() {
+    if (this.speed > maxSpeed) {
+    this.velocity.dx = (this.velocity.dx / this.speed) * maxSpeed;
+    this.velocity.dy = (this.velocity.dy / this.speed) * maxSpeed;
     }
-
-    preyTakeStep() {
-      this.speed = Math.sqrt(this.velocity.dx**2 + this.velocity.dy**2)
-      this.angle = Math.atan2(this.velocity.dy,this.velocity.dx);
-
-      this.preyMinMaxVelocity();
-      this.preyEdgeHandling();
-      this.preySight();
-
-      this.pos.x += this.velocity.dx;
-      this.pos.y += this.velocity.dy;
-      this.preyDrawBoid();
+    if (this.speed < minSpeed) {
+      this.velocity.dx = (this.velocity.dx / this.speed) * minSpeed;
+      this.velocity.dy = (this.velocity.dy / this.speed) * minSpeed;
     }
   }
+
+  preyTakeStep() {
+    this.speed = Math.sqrt(this.velocity.dx**2 + this.velocity.dy**2);
+    this.angle = Math.atan2(this.velocity.dy,this.velocity.dx);
+
+    this.preyMinMaxVelocity();
+    this.preyEdgeHandling();
+
+    if (this.stut > this.Frame_Buffer){
+      this.preySight();
+    }
+    else{
+      this.stut ++;
+    }
+
+    this.pos.x += this.velocity.dx;
+    this.pos.y += this.velocity.dy;
+    this.preyDrawBoid();
+  }
+}
 
 export { PreyBoid };
--- JS/canvas.js
+++ JS/canvas.js
@@ -5,8 +5,51 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext("2d");
 
+let settings = {
+  pause : false,
+  highlightBoid : false,
+  avoidWalls : false,
+  drawLineTo : false,
+  drawVisionCone : false,
+  drawavoidanceCone : false,
+  drawForceVector : false
+};
+
+//Buttons
+document.getElementById("pauseButton").addEventListener('click', function() {
+  settings.pause = !settings.pause;
+});
+document.getElementById("highlightBoidButton").addEventListener('click', function() {
+  settings.highlightBoid = !settings.highlightBoid;
+});
+document.getElementById("avoidWallsButton").addEventListener('click', function() {
+  settings.avoidWalls = !settings.avoidWalls;
+});
+document.getElementById("drawLineButton").addEventListener('click', function() {
+  settings.drawLineTo = !settings.drawLineTo;
+});
+document.getElementById("visionConeButton").addEventListener('click', function() {
+  settings.drawVisionCone = !settings.drawVisionCone;
+});
+document.getElementById("avoidanceConeButton").addEventListener('click', function() {
+  settings.drawavoidanceCone = !settings.drawavoidanceCone;
+});
+document.getElementById("forceVectorButton").addEventListener('click', function() {
+  settings.drawForceVector = !settings.drawForceVector;
+});
+
 var preyArray = [];
-var numPrey = 20;
+var numPrey = 200
+
+var seperationValue = document.getElementById("seperationInput").value;
+var cohesionValue = document.getElementById("cohesionInput").value;
+var alignmentValue = document.getElementById("alignmentInput").value;
+
+var visRadius = document.getElementById("visRadiusInput").value;
+var avoidRadius = document.getElementById("avoidRadiusInput").value;
+var wallAvoidance = document.getElementById("wallAvoidanceInput").value;
+var minSpeed = document.getElementById("minSpeedInput").value;
+var maxSpeed = document.getElementById("maxSpeedInput").value;
 
 function load() {
   for (var i = 0; i < numPrey; i++) {
@@ -16,17 +59,26 @@ function load() {
 }
 
 function update() {
-//  document.onkeypress = function (e) {
-//    e = e || window.event;
-    requestAnimationFrame(update);
-//  };
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(update);
+  if (!settings.pause){
+    seperationValue = document.getElementById("seperationInput").value;
+    cohesionValue = document.getElementById("cohesionInput").value; 
+    alignmentValue = document.getElementById("alignmentInput").value;
 
-  for (var i = 0; i < preyArray.length; i++) {
-    preyArray[i].preyTakeStep();
+    visRadius = document.getElementById("visRadiusInput").value;
+    avoidRadius = document.getElementById("avoidRadiusInput").value;
+    wallAvoidance = document.getElementById("wallAvoidanceInput").value;
+    minSpeed = document.getElementById("minSpeedInput").value;
+    maxSpeed = document.getElementById("maxSpeedInput").value;
+
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < preyArray.length; i++) {
+      preyArray[i].preyTakeStep();
+    }
   }
 }
 
 load();
-export {c, canvas, preyArray};
 update();
+export {c, canvas, preyArray, settings, seperationValue, cohesionValue, alignmentValue, visRadius, avoidRadius, wallAvoidance, maxSpeed, minSpeed};
